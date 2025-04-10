a7847ee1828388474627633dfa449248af618676
Priyanshu Patel <111510461+12princ@users.noreply.github.com>
2025-04-10 23:43:31 +0530

    fixed the import paths in the verifySchemas.ts file

--- sanity/lib/verifySchemas.ts
+++ sanity/lib/verifySchemas.ts
@@ -1,9 +1,9 @@
-import post from '../schemas/post';
-import project from '../schemas/project';
-import service from '../schemas/service';
-import about from '../schemas/about';
-import contact from '../schemas/contact';
-import author from '../schemas/author';
+import post from '../../schemas/post';
+import project from '../../schemas/project';
+import service from '../../schemas/service';
+import about from '../../schemas/about';
+import contact from '../../schemas/contact';
+import author from '../../schemas/author';
 
 // Just a simple verification file - won't actually be used
 // This is to ensure all schemas are properly typed and exported
